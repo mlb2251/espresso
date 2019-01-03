23b9eb378a8aa34d08ac9de431c0a1025c01db4f
Matt Bowers <mlb2251@columbia.edu>
2019-01-03 14:08:51 -0500

    arrow keys and history now work in repl

--- src/main.py
+++ src/main.py
@@ -1,3 +1,24 @@
+
+
+import os
+import readline
+import rlcompleter
+readline.parse_and_bind("tab: complete")
+histfile = os.path.join(os.path.expanduser("~"), ".eshist")
+try:
+    readline.read_history_file(histfile)
+    # default history len is -1 (infinite), which may grow unruly
+    readline.set_history_length(1000)
+except IOError:
+    pass
+import atexit
+atexit.register(readline.write_history_file, histfile)
+del os, histfile, rlcompleter
+
+## apparently theres a way to make your own completer!
+## could have a function that defaults to the python 'rlcompleter' 
+
+
 import sys
 import os
 from importlib import reload
@@ -7,22 +28,25 @@ import util as u
 
 prgm_args = sys.argv[2:]    # often this is []
 
+
+prelude = [
+    "import sys", "import os", "sys.path.append(os.environ['HOME']+'/espresso/src/')", "import backend"
+    ]
+
 if len(sys.argv) > 1:
     infile = sys.argv[1]
     outfile = "a_out.py"
 
-    code = "import sys\n"
-    code += "import os\n"
-    code += "sys.path.append(os.environ['HOME']+'/espresso/src/')\n"
-    code += "import backend\n"
+    code = prelude
 
     with open(infile,'r') as f:
         for line in f.readlines():
-            code += codegen.parse(line)
-    print(u.mk_yellow(code))
+            code.append(codegen.parse(line))
+    result = '\n'.join(code)
+    print(u.mk_yellow(result))
 
     with open(outfile,'w') as f:
-        f.write(code)
+        f.write(result)
 
     u.green("successfully written to:"+outfile)
     u.green("running...")
@@ -44,16 +68,13 @@ else: # REPL mode
     tmpfile = tmpdir+'/a_out.py'
 
     # Prelude
-    code = ["import sys"]
-    code.append("import os")
-    code.append("sys.path.append(os.environ['HOME']+'/espresso/src/')")
-    code.append("import backend")
+    code = prelude
     with open(tmpfile,'w') as f:
         f.write('\n'.join(code))
 
+    import a_out    #initial import so that refresh() can be used in loop
+    code.append("backend.disablePrint() # REMOVE THIS FOR SCRIPT") # this MUST go after 'import' or you wont get any output!
     debug=False
-    import a_out
-    code.append("backend.disablePrint() # REMOVE THIS FOR SCRIPT") # disable print() attempts up until next enablePrint()
     # REPL loop
     while True:
         line = input(u.mk_green(">>> "))
@@ -67,6 +88,10 @@ else: # REPL mode
                 print(u.mk_yellow('\n'.join(code)))
             if line.strip() == '!debug':
                 debug = not debug
+            if line.strip() == '!%':
+                pass
+            if line.strip() == '!sh':
+                pass
             continue
 
         # update codeblock
